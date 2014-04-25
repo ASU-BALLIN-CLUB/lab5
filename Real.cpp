27d1f2bde9fa89eddc8c54b295e79ff00010726a
Shangxin <Kevin.shangxin@gmail.com>
2014-04-25 10:45:05 -0700

    Create Real.cpp

---
+++ Real.cpp
@@ -0,0 +1,20 @@
+//
+//  Real.cpp
+//  
+//
+//  Created by James on 4/22/14.
+//
+//
+
+#include "Real.h"
+#include "Literal.h"
+
+Real::Real() : Literal<float>(0.0){
+    
+}
+Real::~Real(){
+    
+}
+void Real::print(){
+    
+}
