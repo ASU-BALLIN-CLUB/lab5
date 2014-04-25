9be4a37c7b7f1d1b8e53c42e45b3b588e5aac153
Shangxin <Kevin.shangxin@gmail.com>
2014-04-25 10:42:55 -0700

    Create Int.h

---
+++ Int.h
@@ -0,0 +1,23 @@
+//
+//  Int.h
+//  
+//
+//  Created by James on 4/22/14.
+//
+//
+
+#ifndef ____Int__
+#define ____Int__
+
+#include <iostream>
+#include "Literal.h"
+
+class Int : public Literal<int>
+{
+public:
+    Int();
+    ~Int();
+    virtual void print();
+};
+
+#endif /* defined(____Int__) */
