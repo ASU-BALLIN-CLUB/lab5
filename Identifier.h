f5ee3f37f077967b534a4bf7ab793435c581660b
Shangxin <Kevin.shangxin@gmail.com>
2014-04-25 10:41:44 -0700

    Create Identifier.h

---
+++ Identifier.h
@@ -0,0 +1,30 @@
+//
+//  Identifier.h
+//  
+//
+//  Created by James on 4/18/14.
+//
+//
+
+#ifndef ____Identifier__
+#define ____Identifier__
+
+#include <iostream>
+#include "Token.h"
+
+class Identifier : public Token
+{
+private:
+    Identifier *leftChild;
+    Identifier *rightChild;
+    
+public:
+    Identifier();
+    ~Identifier();
+    void setLeftChild(Identifier *tok);
+    Identifier *getLeftChild();
+    void setRightChild(Identifier *tok);
+    Identifier *getRightChild();
+};
+
+#endif /* defined(____Identifier__) */
