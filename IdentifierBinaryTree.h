5c2a439d55184c2171958ba7b258cc8025e91a1b
Shangxin <Kevin.shangxin@gmail.com>
2014-04-25 10:42:18 -0700

    Create IdentifierBinaryTree.h

---
+++ IdentifierBinaryTree.h
@@ -0,0 +1,30 @@
+//
+//  IdentifierBinaryTree.h
+//  Lab4
+//
+//  Created by Bryce Holton on 3/28/14.
+//  Copyright (c) 2014 Bryce Holton. All rights reserved.
+//
+
+#ifndef __Lab4__IdentifierBinaryTree__
+#define __Lab4__IdentifierBinaryTree__
+
+#include <iostream>
+#include "Token.h"
+#include "Identifier.h"
+
+class IdentifierBinaryTree
+{
+    Identifier *treeRoot;
+    
+public:
+    IdentifierBinaryTree();
+    ~IdentifierBinaryTree();
+    void setTreeRoot(Identifier *root);
+    Identifier *getTreeRoot();
+    bool addIdentifier(Identifier *tok, int lineNum);
+    void depthFirstDeleteTree(Identifier *tok);
+    
+};
+
+#endif /* defined(__Lab4__IdentifierBinaryTree__) */
