4b05b2c91ae1d41c6cc9871b4ef7a0388351f067
Shangxin <Kevin.shangxin@gmail.com>
2014-04-25 10:46:06 -0700

    Create String.cpp

---
+++ String.cpp
@@ -0,0 +1,19 @@
+//
+//  String.cpp
+//  
+//
+//  Created by James on 4/22/14.
+//
+//
+
+#include "String.h"
+
+String::String():Literal<string>(NULL){
+    
+}
+String::~String(){
+    
+}
+void String::print(){
+    
+}
