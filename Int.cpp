6de66c368ceefff8f03f2049dce00a36e6f8698f
Shangxin <Kevin.shangxin@gmail.com>
2014-04-25 10:42:34 -0700

    Create Int.cpp

---
+++ Int.cpp
@@ -0,0 +1,19 @@
+//
+//  Int.cpp
+//  
+//
+//  Created by James on 4/22/14.
+//
+//
+
+#include "Int.h"
+#include "Literal.h"
+Int::Int():Literal<int>(0){
+    
+}
+Int::~Int(){
+    
+}
+void Int::print(){
+
+}
