f5e77fc94284a59df9eb46687dab30cf7f3806c4
Shangxin <Kevin.shangxin@gmail.com>
2014-04-25 19:45:49 -0700

    Update Literal.h

--- Literal.h
+++ Literal.h
@@ -13,20 +13,15 @@
 #include "Token.h"
 
 template <typename T>
-class Literal : public Token
-{
+class Literal : public Token{
 private:
     T literal;
-    
 public:
-    Literal(T lit){setLiteral(lit);}
+    Literal(T lit) {setLiteral(lit);}
     ~Literal(){}
-    void setLiteral(T lit){
-        literal = lit;
-    }
-    T getLiteral(){
-        return literal;
-    }
-    virtual void print()=0;
+    void setLiteral(T lit) {literal = lit;}
+    T getLiteral() {return literal;}
+    virtual void print() = 0;
 };
+
 #endif /* defined(____Literal__) */
