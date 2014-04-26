7efdf30b821064a7b76bd3d7c6697da637a0095e
Shangxin <Kevin.shangxin@gmail.com>
2014-04-25 19:49:07 -0700

    Update Token.cpp

--- Token.cpp
+++ Token.cpp
@@ -26,10 +26,6 @@ Token::~Token()
         delete root;
         root = tmp;
     }
-    if (this->getType() == STRING_LIT)
-    {
-        free(this->literal.stringLiteral);
-    }
 }
 void Token::setCode(TokenCode newCode)
 {
@@ -38,39 +34,6 @@ void Token::setCode(TokenCode newCode)
 TokenCode Token::getCode()
 {
     return this->code;
-}
-void Token::setType(Literal newType)
-{
-    this->type = newType;
-}
-Literal Token::getType()
-{
-    return this->type;
-}
-void Token::setLiteral(int newInteger)
-{
-    this->literal.integer = newInteger;
-}
-int Token::getIntLiteral()
-{
-    return this->literal.integer;
-}
-void Token::setLiteral(float newReal)
-{
-    this->literal.real = newReal;
-}
-float Token::getRealLiteral()
-{
-    return this->literal.real;
-}
-void Token::setLiteral(string newString)
-{
-    this->literal.stringLiteral = (char*)malloc(sizeof(char) * newString.length() + 1);
-    strcpy(this->literal.stringLiteral, newString.c_str());
-}
-string Token::getStringLiteral()
-{
-    return string(this->literal.stringLiteral);
 }
 void Token::setTokenString(string s)
 {
