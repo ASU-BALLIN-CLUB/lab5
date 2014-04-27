87325ebb008a17c4975390925f8529dc343d48ef
abalci <abalci@asu.edu>
2014-04-26 23:24:29 -0700

    completed 100%

--- Token.cpp
+++ Token.cpp
@@ -4,9 +4,11 @@
 //
 //  Created by Bryce Holton.
 //
+//  Lab Partners: Sean Slamka, Aydin Balci, James (Shangxin) Wang
 
 #include "Token.h"
 
+// Constant characters of different symbols.
 const char* const SYMBOL_STRINGS[] =
 {
     "<no token>", "<IDENTIFIER>", "<NUMBER>", "<STRING>",
@@ -20,6 +22,7 @@ const char* const SYMBOL_STRINGS[] =
     "VAR","WHILE","WITH",
 };
 
+// Default token constructor.
 Token::Token()
 {
     //What code do I need here to initialize everything.
@@ -27,6 +30,7 @@ Token::Token()
     setRightChild(NULL);
     list = NULL;
 }
+// Token deconstructor to free space.
 Token::~Token()
 {
     //What code do I need here to free memory
@@ -44,72 +48,88 @@ Token::~Token()
         free(this->literal.stringLiteral);
     }
 }
+// Sets the code in the input token.
 void Token::setCode(TokenCode newCode)
 {
     this->code = newCode;
 }
+// Accessor method to retrieve code.
 TokenCode Token::getCode()
 {
     return this->code;
 }
+// Mutator method to access the type.
 void Token::setType(LiteralType newType)
 {
     this->type = newType;
 }
+// Accessor method to retrieve type.
 LiteralType Token::getType()
 {
     return this->type;
 }
+// Mutator method to set the Int Literal.
 void Token::setLiteral(int newInteger)
 {
     this->literal.integer = newInteger;
 }
+// Accessor method to retrieve the Int Literal.
 int Token::getIntLiteral()
 {
     return this->literal.integer;
 }
+// Mutator method to set the real literal.
 void Token::setLiteral(float newReal)
 {
     this->literal.real = newReal;
 }
+// Accessor method to retrieve the Real literal.
 float Token::getRealLiteral()
 {
     return this->literal.real;
 }
+// Mutator method to set the string literal.
 void Token::setLiteral(string newString)
 {
     this->literal.stringLiteral = (char*)malloc(sizeof(char) * newString.length() + 1);
     strcpy(this->literal.stringLiteral, newString.c_str());
 }
+// Accessor method to retrieve the string literal.
 string Token::getStringLiteral()
 {
     return string(this->literal.stringLiteral);
 }
+// Mutator method to set the Token string.
 void Token::setTokenString(string s)
 {
     this->tokenString = s;
 }
+// Accessor method to retrieve the token string.
 string Token::getTokenString()
 {
     return this->tokenString;
 }
-//What methods am I missing to implement a binary tree.
+// Mutator method to set the left child of the token.
 void Token::setLeftChild(Token *tok)
 {
     this->leftChild = tok;
 }
+// Accessor method to retrieve the left child of the token.
 Token *Token::getLeftChild()
 {
     return this->leftChild;
 }
+// Mutator method to set the right child of the token
 void Token::setRightChild(Token *tok)
 {
     this->rightChild = tok;
 }
+// Accessor method to retrieve the right child of the token.
 Token *Token::getRightChild()
 {
     return this->rightChild;
 }
+// Method to add the *list item to the Line Number List.
 void Token::addToLineNumberList(LineNumberList *listItem)
 {
     LineNumberList *tmp = getLineNumberList();
@@ -127,10 +147,12 @@ void Token::addToLineNumberList(LineNumberList *listItem)
         tmp->setNextLineNumber(listItem);
     }
 }
+// Gets the line number list.
 LineNumberList *Token::getLineNumberList()
 {
     return this->list;
 }
+// Basic Print function
 void Token::print()
 {
 	const char *symbol_string = SYMBOL_STRINGS[this->getCode()];
