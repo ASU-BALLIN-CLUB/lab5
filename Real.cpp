55ce8ab399b73de4a97df869dfb7fb010cda4d63
abalci <abalci@asu.edu>
2014-04-26 19:54:50 -0700

    Completed + Commented

--- Real.cpp
+++ Real.cpp
@@ -4,17 +4,20 @@
 //
 //  Created by James on 4/22/14.
 //
-//
+//  Lab Partners: Sean Slamka, Aydin Balci, James (Shangxin) Wang
 
 #include "Real.h"
 #include "Literal.h"
 
+// real literal constructor
 Real::Real() : Literal<float>(0.0){
     
 }
+// Deconstructor
 Real::~Real(){
     
 }
+// Print function for real literal.
 void Real::print(){
     cout << "    >> <NUMBER>" << this->getLiteral() << " (real)\n";
 }
