8d32c7e98b83432c0cc239871d31b8ff8dbb71f8
abalci <abalci@asu.edu>
2014-04-26 19:55:20 -0700

    Completed + Commented

--- String.cpp
+++ String.cpp
@@ -4,16 +4,19 @@
 //
 //  Created by James on 4/22/14.
 //
-//
+//  Lab Partners: Sean Slamka, Aydin Balci, James (Shangxin) Wang
 
 #include "String.h"
 
+// String Constructor
 String::String():Literal<string>(NULL){
     
 }
+// String literal deconstructor
 String::~String(){
     
 }
+// String literal print function.
 void String::print(){
     cout << "    >> <STRING>" << this->getLiteral() << "\n";
 }
