e2decc63f72a0682e34944f1ec680e543d4bb593
abalci <abalci@asu.edu>
2014-04-26 23:10:56 -0700

    completed 100%

--- LineNumberList.h
+++ LineNumberList.h
@@ -5,12 +5,14 @@
 //  Created by Bryce Holton on 3/28/14.
 //  Copyright (c) 2014 Bryce Holton. All rights reserved.
 //
+//  Lab Partners: Sean Slamka, Aydin Balci, James (Shangxin) Wang
 
 #ifndef __Lab4__LineNumberList__
 #define __Lab4__LineNumberList__
 
 #include <iostream>
 
+// Header file for the LineNumberList (contains a line number integer, then next in the list)
 class LineNumberList
 {
     int lineNumber;
