885c8378943761df7b8fda5881c6d11d315295e5
Shangxin <Kevin.shangxin@gmail.com>
2014-04-26 22:52:37 -0700

    need comment

--- Token.cpp
+++ Token.cpp
@@ -7,6 +7,19 @@
 
 #include "Token.h"
 
+const char* const SYMBOL_STRINGS[] =
+{
+    "<no token>", "<IDENTIFIER>", "<NUMBER>", "<STRING>",
+    "^","*","(",")","-","+","=","[","]",":",";",
+    "<",">",",",".","/",":=","<=",">=","<>","..",
+    "<END OF FILE>", "<ERROR>",
+    "AND","ARRAY","BEGIN","CASE","CONST","DIV","DO","DOWNTO",
+    "ELSE","END","FILE","FOR","FUNCTION","GOTO","IF","IN",
+    "LABEL","MOD","NIL","NOT","OF","OR","PACKED","PROCEDURE",
+    "PROGRAM","RECORD","REPEAT","SET","THEN","TO","TYPE","UNTIL",
+    "VAR","WHILE","WITH",
+};
+
 Token::Token()
 {
     //What code do I need here to initialize everything.
@@ -26,6 +39,10 @@ Token::~Token()
         delete root;
         root = tmp;
     }
+    if (this->getType() == STRING_LIT)
+    {
+        free(this->literal.stringLiteral);
+    }
 }
 void Token::setCode(TokenCode newCode)
 {
@@ -34,6 +51,39 @@ void Token::setCode(TokenCode newCode)
 TokenCode Token::getCode()
 {
     return this->code;
+}
+void Token::setType(LiteralType newType)
+{
+    this->type = newType;
+}
+LiteralType Token::getType()
+{
+    return this->type;
+}
+void Token::setLiteral(int newInteger)
+{
+    this->literal.integer = newInteger;
+}
+int Token::getIntLiteral()
+{
+    return this->literal.integer;
+}
+void Token::setLiteral(float newReal)
+{
+    this->literal.real = newReal;
+}
+float Token::getRealLiteral()
+{
+    return this->literal.real;
+}
+void Token::setLiteral(string newString)
+{
+    this->literal.stringLiteral = (char*)malloc(sizeof(char) * newString.length() + 1);
+    strcpy(this->literal.stringLiteral, newString.c_str());
+}
+string Token::getStringLiteral()
+{
+    return string(this->literal.stringLiteral);
 }
 void Token::setTokenString(string s)
 {
@@ -60,9 +110,6 @@ Token *Token::getRightChild()
 {
     return this->rightChild;
 }
-void Token::print()
-{
-}
 void Token::addToLineNumberList(LineNumberList *listItem)
 {
     LineNumberList *tmp = getLineNumberList();
@@ -83,4 +130,9 @@ void Token::addToLineNumberList(LineNumberList *listItem)
 LineNumberList *Token::getLineNumberList()
 {
     return this->list;
+}
+void Token::print()
+{
+	const char *symbol_string = SYMBOL_STRINGS[this->getCode()];
+	cout << "    >> " << symbol_string << "\t\t" << this->getTokenString() << "\n";
 }
