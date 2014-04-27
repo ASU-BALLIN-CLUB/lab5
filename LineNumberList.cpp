05749a281123453942bd3f48e57b1887750e30be
abalci <abalci@asu.edu>
2014-04-26 19:53:39 -0700

    Completed + Commented

--- LineNumberList.cpp
+++ LineNumberList.cpp
@@ -8,27 +8,33 @@
 
 #include "LineNumberList.h"
 
+// LineNumberList Constructor
 LineNumberList::LineNumberList()
 {
     setLineNumber(0);
     setNextLineNumber(NULL);
 }
+// LineNumberList Deconstructor
 LineNumberList::~LineNumberList()
 {
     
 }
+// Sets the line number to specified input.
 void LineNumberList::setLineNumber(int num)
 {
     this->lineNumber = num;
 }
+// LineNumber Accessor method
 int LineNumberList::getLineNumber()
 {
     return this->lineNumber;
 }
+// NextLineNumber mutator method
 void LineNumberList::setNextLineNumber(LineNumberList *next)
 {
     this->nextLineNumber = next;
 }
+// NextLineNumber Accessor method
 LineNumberList *LineNumberList::getNextLineNumber()
 { 
     return this->nextLineNumber;
