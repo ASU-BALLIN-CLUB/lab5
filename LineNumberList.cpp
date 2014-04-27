97f14592b5e8ce7550c4dd7477c25ecae05e86b6
Shangxin <Kevin.shangxin@gmail.com>
2014-04-26 22:50:10 -0700

    need comment

--- LineNumberList.cpp
+++ LineNumberList.cpp
@@ -5,39 +5,31 @@
 //  Created by Bryce Holton on 3/28/14.
 //  Copyright (c) 2014 Bryce Holton. All rights reserved.
 //
-//  Lab Partners: Sean Slamka, Aydin Balci, James (Shangxin) Wang
 
 #include "LineNumberList.h"
 
-// LineNumberList Constructor
 LineNumberList::LineNumberList()
 {
     setLineNumber(0);
     setNextLineNumber(NULL);
 }
-// LineNumberList Deconstructor
 LineNumberList::~LineNumberList()
 {
     
 }
-// Sets the line number to specified input.
 void LineNumberList::setLineNumber(int num)
 {
     this->lineNumber = num;
 }
-// LineNumber Accessor method
 int LineNumberList::getLineNumber()
 {
     return this->lineNumber;
 }
-// NextLineNumber mutator method
 void LineNumberList::setNextLineNumber(LineNumberList *next)
 {
     this->nextLineNumber = next;
 }
-// NextLineNumber Accessor method
 LineNumberList *LineNumberList::getNextLineNumber()
-{ 
+{
     return this->nextLineNumber;
 }
-
