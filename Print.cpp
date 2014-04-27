9ea47207996e704de99720763e109c05053e4370
Shangxin <Kevin.shangxin@gmail.com>
2014-04-26 22:50:59 -0700

    need comment

--- Print.cpp
+++ Print.cpp
@@ -6,26 +6,6 @@
 //
 
 #include "Print.h"
-#include "Token.h"
-#include "Int.h"
-#include "Real.h"
-#include "String.h"
-#include "Literal.h"
-#include "common.h"
-
-
-const char* const SYMBOL_STRINGS[] =
-{
-    "<no token>", "<IDENTIFIER>", "<NUMBER>", "<STRING>",
-    "^","*","(",")","-","+","=","[","]",":",";",
-    "<",">",",",".","/",":=","<=",">=","<>","..",
-    "<END OF FILE>", "<ERROR>",
-    "AND","ARRAY","BEGIN","CASE","CONST","DIV","DO","DOWNTO",
-    "ELSE","END","FILE","FOR","FUNCTION","GOTO","IF","IN",
-    "LABEL","MOD","NIL","NOT","OF","OR","PACKED","PROCEDURE",
-    "PROGRAM","RECORD","REPEAT","SET","THEN","TO","TYPE","UNTIL",
-    "VAR","WHILE","WITH",
-};
 
 Print::Print(char source_name[], char date[])
 {
@@ -44,24 +24,24 @@ void Print::printLine(char line[])
     char *save_chp = NULL;
     
     if (++lineCount > MAX_LINES_PER_PAGE)
-    {
+	{
         printPageHeader();
         lineCount = 1;
-    }
+	}
     if (strlen(line) > MAX_PRINT_LINE_LENGTH)
-    {
+	{
         save_chp = &line[MAX_PRINT_LINE_LENGTH];
-    }
+	}
     if (save_chp)
-    {
+	{
         save_ch = *save_chp;
         *save_chp = '\0';
-    }
+	}
     printf("%s", line);
     if (save_chp)
-    {
+	{
         *save_chp = save_ch;
-    }
+	}
 }
 void Print::printPageHeader()
 {
@@ -70,50 +50,50 @@ void Print::printPageHeader()
 }
 void Print::printToken(Token *token)
 {
-    char line[MAX_SOURCE_LINE_LENGTH + 32];
-    const char *symbol_string = SYMBOL_STRINGS[token->getCode()];
+    //bool numberType;
+	//puts("q");
+    //char line[MAX_SOURCE_LINE_LENGTH + 32];
+    //puts("r");
+    //const char *symbol_string = SYMBOL_STRINGS[token->getCode()];
     //token = new Integer();
     //Real *newReal = new Real();
     //String *newString = new String();
+	//puts("s");
     
     
-    switch (token->getCode())
-    {
-        case NUMBER:
-            if (numberType)
-            {
-                //                sprintf(line, "    >> %-16s %d (integer)\n", symbol_string, token->getIntLiteral());
-                token = new Int();
+    switch (token->getCode()) {
+        { case NUMBER:
+            if (number_type) {
+                Int* token = new Int();
                 token->print();
-                
-            }
-            else
-            {
-                //                sprintf(line, "    >> %-16s %g (real)\n", symbol_string, token->getRealLiteral());
-                token = new Real();
+            } else {
+                Real* token = new Real();
                 token->print();
             }
-            //            intToken->print(line, symbol_string);
-            
-            
             break;
-        case STRING:
-            //            sprintf(line, "    >> %-16s %-s\n", symbol_string, token->getStringLiteral().c_str());
-            token = new String();
+        }
+        {
+            case STRING:
+            String* token = new String();
             token->print();
+            break;
             
+        }
+        {
+            default:
+            //sprintf(line, "    >> %-16s %-s\n", symbol_string, token->getTokenString().c_str());
             break;
-        default:
-            sprintf(line, "    >> %-16s %-s\n", symbol_string, token->getTokenString().c_str());
-            break;
+        }
     }
-    printLine(line);
+    
+    token->print();
 }
+
 int Print::getLineCount()
 {
     return this->lineCount;
 }
-void Print::printTreeRecursive(Token *identifier)
+void Print::printTreeRecursive(Identifier *identifier)
 {
     char line[MAX_SOURCE_LINE_LENGTH + 32];
     
@@ -136,7 +116,7 @@ void Print::printTreeRecursive(Token *identifier)
         printTreeRecursive(identifier->getRightChild());
     }
 }
-void Print::printTree(Token *identifier)
+void Print::printTree(Identifier *identifier)
 {
     cout << "\n Cross Reference Information\n";
     cout << " Identifier \t\tLine Numbers\n";
