200e3b4ae99de01d34662b987bf77b96d7dd0ee6
abalci <abalci@asu.edu>
2014-04-26 18:28:14 -0700

    Completed + Commented

--- Int.cpp
+++ Int.cpp
@@ -4,16 +4,20 @@
 //
 //  Created by James on 4/22/14.
 //
-//
+//  Lab Partners: Sean Slamka, Aydin Balci, James (Shangxin) Wang
 
 #include "Int.h"
 #include "Literal.h"
+
+// Int constructor
 Int::Int():Literal<int>(0){
     
 }
+// Int Deconstructor
 Int::~Int(){
     
 }
+// Print function for integer literals.
 void Int::print(){
     cout << "    >> <NUMBER>" << this->getLiteral() << " (integer)\n";
 }
