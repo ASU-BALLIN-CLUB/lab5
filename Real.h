db520a1e7589bacd3464540e2097f659cc1366b6
abalci <abalci@asu.edu>
2014-04-26 19:58:05 -0700

    Completed + Commented

--- Real.h
+++ Real.h
@@ -4,7 +4,7 @@
 //
 //  Created by James on 4/22/14.
 //
-//
+//  Lab Partners: Sean Slamka, Aydin Balci, James (Shangxin) Wang
 
 #ifndef ____Real__
 #define ____Real__
@@ -14,6 +14,7 @@
 
 using namespace std;
 
+// Header file for Real literal.
 class Real : public Literal<float>
 {
 public:
